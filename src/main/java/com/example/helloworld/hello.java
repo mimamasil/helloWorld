d6f81acf19181ad660291c232e3cff60546196c7
mimamasil <123141377+mimamasil@users.noreply.github.com>
2023-02-19 21:07:04 +0800

    hello

---
+++ src/main/java/com/example/helloworld/hello.java
@@ -0,0 +1,12 @@
+package com.example.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/")
+public class hello {
+    @GetMapping
+    public String helloWorld() {
+        return "Hello";
+    }
+}
